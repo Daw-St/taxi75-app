44bd36ae30c88cc03c17c67b19ec00e2f1279334
Wojciech Najman <wojciech.najman@gmail.com>
2019-05-05 17:30:24 +0200

    bugfixes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import App from './components/App';
 import { Provider } from 'react-redux'
 import store from './store';
 
-function render() {
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector('#root'));
-}
-
-store.subscribe(render);
-
-render();
+ReactDOM.render(<Provider store={store}><App/></Provider>,
+    document.querySelector('#root'));
